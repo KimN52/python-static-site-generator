caad9252f0d5de0ed223e41de8a39aa501ff803e
Kim N <k.nendels@outlook.com>
2022-01-22 13:02:50 +0100

    Completed module

---
+++ ssg/site.py
@@ -0,0 +1,18 @@
+import Path from pathlib
+
+class Site(object):
+    """docstring for Site."""
+
+    def __init__(self, source, dest):
+        self._source = Path(source)
+        self._dest = Path(dest)
+
+    def create_dir(self, path):
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
+
+    def build(self):
+        self.dest.mkdir(parents=True, exists_ok=True)
+        for path in self.source.rglob("*"):
+            if path.is_dir():
+                self.create_dir(path)         
