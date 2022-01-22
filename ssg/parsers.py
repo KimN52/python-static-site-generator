5aabd93235260c79881c4eca1d6906390064aaaa
Kim N <k.nendels@outlook.com>
2022-01-22 13:40:17 +0100

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -4,3 +4,9 @@ from pathlib import Path
 class Parser(object):
     """docstring for Parser."""
     extensions: List[str] =[]
+
+    def valid_extensions(self, extension):
+        return extension in self.extensions
+
+    def parse(path:Path, source:Path, dest:Path)
+        raise NotImplementedError
